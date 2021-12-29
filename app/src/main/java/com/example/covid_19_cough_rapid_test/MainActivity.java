7c2e9cbcda0aaf9d6422f453962a9d7d24f06058
chgeske <chgeske@techfak.de>
2021-12-29 15:43:09 +0100

    Added a simple ML functionality to the app as a proof of concept. The funcionality that was added is a toy example trained with Google Colab. See ShuklaAnuja/Python-ML---Android-Kit for the example used.

--- app/src/main/java/com/example/covid_19_cough_rapid_test/MainActivity.java
+++ app/src/main/java/com/example/covid_19_cough_rapid_test/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.covid_19_cough_rapid_test;
 
+import android.content.res.AssetFileDescriptor;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -17,11 +18,29 @@ import com.example.covid_19_cough_rapid_test.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+
+    // Interpreter interprets the input and output of data using the ML model
+    Interpreter interpreter;
+
+    EditText editText;
+    TextView textView;
+    Button button;
+
 
     // Called when the application is starting, before any other application objects have been created
     @Override
@@ -37,11 +56,30 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+
+
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        // ML functionality
+        editText = findViewById(R.id.inputX);
+        button = findViewById(R.id.predictbtn);
+        textView = findViewById(R.id.outputY);
+        try {
+            interpreter = new Interpreter(loadModelFile(),null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float f =  doInference(editText.getText().toString());
+                textView.setText("Result - "+f);
             }
         });
     }
@@ -73,5 +111,30 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    // ML functionality from https://github.com/ShuklaAnuja/Python-ML---Android-Kit/blob/main/MainActivity.java and https://medium.com/geekculture/train-ml-model-and-build-android-application-using-tensorflow-lite-keras-6bf23d07309a
+    // Converts tflite model file into MappedByteBuffer format which is an optimized format = increase efficiency and speed
+    private MappedByteBuffer loadModelFile() throws IOException
+    {
+        AssetFileDescriptor assetFileDescriptor = this.getAssets().openFd("linear.tflite"); // .tflite found in assets directory. Downloaded from Google colab
+        FileInputStream fileInputStream = new FileInputStream(assetFileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = fileInputStream.getChannel();
+
+        long startOffset = assetFileDescriptor.getStartOffset();
+        long len = assetFileDescriptor.getLength();
+
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startOffset,len);
+    }
+
+    // ML functionallity
+    public float doInference(String val)
+    {
+        float[] input = new float[1];
+        input[0] = Float.parseFloat(val);
+        float[][] output = new float[1][1];
+
+        interpreter.run(input,output);
+        return  output[0][0];
     }
 }
